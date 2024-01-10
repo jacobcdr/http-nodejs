102b8b6c707dbb03f775d0ea639a324c85eb9b3c
Jacob Ceder <jacob.ceder@husqvarnagroup.com>
2024-01-10 15:30:21 +0100

    commit3

--- server.mjs
+++ server.mjs
@@ -1,19 +1,12 @@
 import { createServer } from 'http';
-import { createReadStream } from 'fs';
+import { readFileSync } from 'fs';
 import { join } from 'path';
 
-createServer((req, res) => {
-  // Kontrollera om förfrågan pekar på roten
-  if (req.url === '/') {
-    // Ange innehållstypen till HTML
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+// Läs innehållet i index.html en gång vid start
+const indexPath = join(__dirname, 'index.html');
+const indexContent = readFileSync(indexPath, 'utf8');
 
-    // Skapa en läsström för index.html och skicka den till responsen
-    const stream = createReadStream(join(__dirname, 'index.html'));
-    stream.pipe(res);
-  } else {
-    // Hantera andra URL:er eller skicka en 404-sida
-    res.writeHead(404);
-    res.end('404 Not Found');
-  }
-}).listen(process.env.PORT);
+createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(indexContent);
+}).listen(process.env.PORT || 3000);
